548b6d7e172dbe74c6b727612ddd087277b03bd9
Shivam Chourey <34999769+ShivamChourey@users.noreply.github.com>
2019-10-02 21:46:06 -0400

    Update Image_geotag_filter.py

--- Image_geotag_filter.py
+++ Image_geotag_filter.py
@@ -2,7 +2,7 @@
 # Shivam Chourey
 # This file reads all the '.JPG' files in a folder and reads their geotags
 # If the latitude and longitude of the image geotags are within a defined range
-# then the file is copied to a subfolder "filtered" created in the same directory as the script file
+# then the file is copied to a subfolder "filtered" created in the current directory
 
 import os
 import glob
